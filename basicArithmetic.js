3a824fbb8ddd392822b428553ba1e76a58d752e6
Miftahul Janna <miftahuljna127@gmail.com>
2022-06-22 21:57:34 +0700

    Add basic operations
     Addition, Subtraction, Multiplication,  Division

---
+++ basicArithmetic.js
@@ -0,0 +1,13 @@
+let addition = 5+7;
+let substraction = 7-4;
+let multiplication = 5*9;
+let division = 24/4;
+
+
+console.log(
+  "5 + 7 = "+ addition +
+  "\n7 - 4 = "+ substraction + 
+  "\n5 * 9 = " + multiplication + 
+  "\n24 / 4 = "+ division
+);
+
